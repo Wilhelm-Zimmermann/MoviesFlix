bab716a6df9ae37936d1c8cd2cabec9dbc0e01c5
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2023-05-17 14:18:29 -0300

    Initial Setup

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { router } from "./http/routes";
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(router);
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
