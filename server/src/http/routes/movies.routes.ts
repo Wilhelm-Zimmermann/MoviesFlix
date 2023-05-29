8da5c9f4f1e5c7915889439112d3866f489c3da4
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2023-05-29 17:01:12 -0300

    needs to be authenticated to create movie

--- server/src/http/routes/movies.routes.ts
+++ server/src/http/routes/movies.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { MoviesController } from "../../modules/movies/useCases/MoviesController";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 const moviesRouter = express.Router();
 
 const moviesController = new MoviesController();
@@ -7,7 +8,7 @@ const moviesController = new MoviesController();
 moviesRouter.get("/movies", moviesController.getAllMovies);
 moviesRouter.get("/movies/rated", moviesController.getRatedMovies);
 moviesRouter.get("/movies/search", moviesController.findMovieByQuery);
-moviesRouter.get("/movies/details/:movieId", moviesController.getMovieDetail);
-moviesRouter.post("/movies/create", moviesController.createMovie);
+moviesRouter.get("/movies/details/:movieId",ensureAuthenticated, moviesController.getMovieDetail);
+moviesRouter.post("/movies/create", ensureAuthenticated,  moviesController.createMovie);
 
 export { moviesRouter };
